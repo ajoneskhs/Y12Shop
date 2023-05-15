11ab4b7143a5855675e6e87071ae2add1477b870
HaydenSyme <Haydensyme@icloud.com>
2023-05-15 09:17:33 +0100

    Added interface
    Added notes and interface to the files explaining what the code will do.

--- Hpayment_processing.py
+++ Hpayment_processing.py
@@ -1,3 +1,15 @@
+#//Check Details\\#
+# The check details function validates the payment information provided. it needs a payment type e.g. 'card'
+# The check details function also needs a list with the details such as cardnumber, CCV, expiry date, etc.
+
+#//Charge\\#
+# The charge function needs a price, stored in the custom variable of currency including the price and the currency
+# The payment details are also needed once again to make the payment
+
+#//Confirmation\\#
+# The conformation function interprets the response from the payment provider, confirming if the payment failed or went through
+# It will return the output from the payment provider, allowing an appropriate error to be displayed to the user.
+
 class Payment:
     def __init__(self):
         pass
--- Hrecommendations.py
+++ Hrecommendations.py
@@ -1,3 +1,11 @@
+#//Common Themes\\#
+# The getCommonThemes function uses the list of the users previously purchased and uses the catagories of those products to recommend products
+# An extension to this code would allow it to display currently trending items to be recommended to new users
+
+#//Simlair Products\\#
+# The getSimilairProducts function retrieves products from the database with the same or similair catagories to the users previosuly purchased.
+# The function will not recommend products a user has already purchased before, this relies on it being in their previosuly purchased list.
+
 class Recommend:
     def __init__(self):
         pass
@@ -5,5 +13,5 @@ class Recommend:
     def getCommonThemes(PreviouslyPurchased: list[str]):
         pass
 
-    def getSimilairProducts(SimilairProducts: list[str]):
+    def getSimilairProducts(SimilairProducts: list[str], PreviouslyPurchase: list[str]):
         pass
