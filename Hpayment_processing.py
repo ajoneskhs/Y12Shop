e77f587558609328446db57eb8a12fcfd027481f
HaydenSyme <Haydensyme@icloud.com>
2023-05-15 08:58:27 +0100

    File creation

---
+++ Hpayment_processing.py
@@ -0,0 +1,13 @@
+class Payment:
+    def __init__(self):
+        pass
+
+    def checkDetails(paymentType: str, paymentDetails: list[str, int, float]):
+        pass
+
+    def charge(self, price: Currency, paymentDetails: list[str, int, float]):
+        pass
+
+    def confirmation(self):
+        pass
+ 
---
+++ Hrecommendations.py
@@ -0,0 +1,9 @@
+class Recommend:
+    def __init__(self):
+        pass
+
+    def getCommonThemes(PreviouslyPurchased: list[str]):
+        pass
+
+    def getSimilairProducts(SimilairProducts: list[str]):
+        pass
