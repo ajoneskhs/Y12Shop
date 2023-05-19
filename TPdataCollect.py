5e8e0dde8ed3d011bd09b68748a8eeb32182ca55
tpyecode <tomspye1@gmail.com>
2023-05-19 10:03:57 +0100

    Data Collector

---
+++ TPdataCollect.py
@@ -0,0 +1,16 @@
+
+class DataCollector:
+  def __init__(self):
+    
+    pass
+  def recordEvent(self,orginModule, ip, instruction, isMajor):
+    from Zdb import databaseConnector
+
+    pass
+
+#  def accounts(self, ):
+#    pass
+#def loginActivity(self, ):
+#    pass
+#def productsDifference(self, ):
+#   pass
