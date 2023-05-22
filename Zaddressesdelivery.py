64f00c7d171fc2c3a1c38105b5b38fdfd7324887
ZSTaylor05 <119850277+ZSTaylor05@users.noreply.github.com>
2023-05-22 09:00:13 +0100

    Update Zaddressesdelivery.py

--- Zaddressesdelivery.py
+++ Zaddressesdelivery.py
@@ -5,13 +5,14 @@ class AddressFinder:
         x = dbConnector()
         self.connector = x.connect()
 
-    def postcode2town(self, postcode: str):
+    def postcodeFinder(self, postcode: str):
         self.connector.execute("SELECT * FROM postcodes WHERE postcode = '"+postcode+"'")
         data = self.connector.fetchall()
         for line in data:
             print(line)
-x = AddressFinder()
-x.postcode2town("TW8")
+
+#x = AddressFinder()
+#x.postcode2town("TW8")
 
 
 
